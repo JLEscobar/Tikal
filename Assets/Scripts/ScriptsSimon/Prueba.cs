0b05505f23cc845419f1f8025064d3e5d9ab0553
SimonsinL <92690782+SimonsinL@users.noreply.github.com>
2025-11-25 17:13:52 -0500

    Well yeah

---
+++ Assets/Scripts/ScriptsSimon/Prueba.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Prueba : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Método para probar el Animation Event
+    public void RocaAttack()
+    {
+        Debug.Log("Animation Event ejecutado en Prueba.");
+    }
+}
